43237f1ae1985c29a47b44366f7fa0379a6b93b4
Fattcat - but not fat cat <109801079+Fattcat@users.noreply.github.com>
2025-11-03 23:24:21 +0100

    Update AttinyTVB-Gone-TV-Blaster.ino

--- AttinyTVB-Gone-TV-Blaster.ino
+++ AttinyTVB-Gone-TV-Blaster.ino
@@ -1,130 +1,66 @@
-// TV + Projector Power-OFF IR Blaster for ATtiny85
-// Supports: Samsung, LG, Panasonic, Sony, Hisense, TCL, Philips, Toshiba, Xiaomi
-//           Epson, BenQ, Optoma, Acer, LG Projector, ViewSonic
+#include <IRremote.h>
 
-// Attiny85 Kód pre DIY TVB-Gone Power OFF Codes
+// Definuj pin pre IR vysielač
+const int irPin =    3; // PB3
+const int GreenLED = 4; // PB4
 
+IRsend irsend; // Objekt pre IR vysielač
 
-// IR LED on PB0 (pin 5)
-
-#define IRledPin 3 // Physical pin number 2 (PB3)
-#define NUM_CODES 15
-
-#include <avr/pgmspace.h>
-
-// RAW codes stored in PROGMEM (each ends with 0 as terminator)
-const unsigned int samsung_power[] PROGMEM = {
-  4500,4500,560,1690,560,1690,560,1690,560,560,560,560,560,560,560,560,560,560,560,1690,560,560,560,560,560,560,560,1690,560,1690,560,1690,560,1690,560,1690,560,1690,560,560,560,1690,560,0
-};
-
-const unsigned int lg_power[] PROGMEM = {
-  9000,4500,560,1690,560,560,560,560,560,560,560,560,560,560,560,1690,560,560,560,560,560,560,560,1690,560,1690,560,1690,560,1690,560,1690,560,0
-};
-
-const unsigned int panasonic_power[] PROGMEM = {
-  3500,1750,440,1300,440,1300,440,1300,440,440,440,440,440,440,440,1300,440,440,440,440,440,440,440,1300,440,1300,440,1300,440,1300,440,1300,440,1300,440,440,440,1300,440,0
-};
-
-const unsigned int sony_power[] PROGMEM = {
-  2400,600,600,600,600,600,600,600,600,600,600,600,600,600,600,600,600,600,600,0
-};
-
-const unsigned int hisense_power[] PROGMEM = {
-  9000,4500,560,1690,560,560,560,560,560,1690,560,560,560,1690,560,1690,560,1690,560,560,560,1690,560,0
-};
-
-const unsigned int tcl_power[] PROGMEM = {
-  9000,4500,560,1690,560,560,560,560,560,1690,560,560,560,1690,560,1690,560,1690,560,560,560,1690,560,0
-};
-
-const unsigned int philips_power[] PROGMEM = {
-  8800,4400,550,1650,550,550,550,550,550,1650,550,550,550,1650,550,1650,550,1650,550,550,550,1650,550,0
-};
-
-const unsigned int toshiba_power[] PROGMEM = {
-  4400,4400,550,1650,550,550,550,550,550,550,550,1650,550,550,550,550,550,1650,550,1650,550,1650,550,1650,550,0
-};
-
-const unsigned int xiaomi_power[] PROGMEM = {
-  9000,4500,560,1690,560,560,560,560,560,1690,560,560,560,1690,560,1690,560,1690,560,560,560,1690,560,0
-};
-
-// Projectors
-const unsigned int epson_power[] PROGMEM = {
-  4500,4500,560,1690,560,560,560,1690,560,560,560,1690,560,1690,560,1690,560,560,560,1690,560,0
-};
-
-const unsigned int benq_power[] PROGMEM = {
-  9000,4500,560,1690,560,560,560,560,560,1690,560,560,560,1690,560,1690,560,1690,560,560,560,1690,560,0
-};
-
-const unsigned int optoma_power[] PROGMEM = {
-  4500,4500,560,1690,560,560,560,560,560,1690,560,560,560,1690,560,1690,560,1690,560,560,560,1690,560,0
-};
-
-const unsigned int acer_power[] PROGMEM = {
-  9000,4500,560,1690,560,560,560,560,560,1690,560,560,560,1690,560,1690,560,1690,560,560,560,1690,560,0
-};
-
-const unsigned int lg_projector_power[] PROGMEM = {
-  9000,4500,560,1690,560,560,560,560,560,560,560,560,560,560,560,1690,560,560,560,560,560,560,560,1690,560,1690,560,1690,560,1690,560,1690,560,0
-};
-
-const unsigned int viewsonic_power[] PROGMEM = {
-  4500,4500,560,1690,560,560,560,1690,560,560,560,1690,560,1690,560,1690,560,560,560,1690,560,0
-};
-
-// Array of pointers to codes
-const unsigned int* const codeList[NUM_CODES] PROGMEM = {
-  samsung_power, lg_power, panasonic_power, sony_power, hisense_power,
-  tcl_power, philips_power, toshiba_power, xiaomi_power,
-  epson_power, benq_power, optoma_power, acer_power,
-  lg_projector_power, viewsonic_power
-};
-
-// --- IR functions ---
-void pulseIR(unsigned int microsecs) {
-  while (microsecs > 0) {
-    digitalWrite(IRledPin, HIGH);
-    delayMicroseconds(13);
-    digitalWrite(IRledPin, LOW);
-    delayMicroseconds(13);
-    if (microsecs <= 26) break;
-    microsecs -= 26;
-  }
-}
-
-void sendRawCode(const unsigned int* code) {
-  unsigned int buffer[150];
-  int len = 0;
-  while (true) {
-    unsigned int val = pgm_read_word_near(code + len);
-    if (val == 0) break;
-    buffer[len] = val;
-    len++;
-    if (len >= 150) break;
-  }
-
-  for (int i = 0; i < len; i++) {
-    if (i % 2 == 0) {
-      pulseIR(buffer[i]);  // ON
-    } else {
-      delayMicroseconds(buffer[i]); // OFF
-    }
-  }
-  delay(50); // inter-command gap
+void blinkGreenLED() {
+  digitalWrite(GreenLED, HIGH);
+  delay(100); // LED svieti 100 ms
+  digitalWrite(GreenLED, LOW);
 }
 
 void setup() {
-  pinMode(IRledPin, OUTPUT);
-  digitalWrite(IRledPin, LOW);
+  pinMode(GreenLED, OUTPUT); // Nastav LED pin ako výstup
+  digitalWrite(GreenLED, LOW); // Zaisti, že LED je na začiatku vypnutá
+  // irsend.begin(); // Nepoužíva sa v IRremote 2.6.1
 }
 
 void loop() {
-  for (int i = 0; i < NUM_CODES; i++) {
-    const unsigned int* code = (const unsigned int*)pgm_read_word_near(codeList + i);
-    sendRawCode(code);
-    delay(100); // medzi značkami
-  }
-  delay(1800); // čakaj ~2s pred opakovaním
+  // AB CryptoBox Power OFF
+  irsend.sendNEC(0x20DF10EF, 32);
+  blinkGreenLED();
+  delay(500);
+
+  // Samsung TV Power OFF
+  irsend.sendNEC(0xE0E040BF, 32);
+  blinkGreenLED();
+  delay(500);
+
+  // LG TV Power OFF
+  irsend.sendNEC(0x20DF40BF, 32);
+  blinkGreenLED();
+  delay(500);
+
+  // Sony TV Power OFF
+  irsend.sendSony(0xA90, 12);
+  blinkGreenLED();
+  delay(500);
+
+  // Panasonic TV Power OFF
+  irsend.sendPanasonic(0xB0E0, 16);
+  blinkGreenLED();
+  delay(500);
+
+  // Sharp TV Power OFF
+  irsend.sendSharp(0x2A5, 16);
+  blinkGreenLED();
+  delay(500);
+
+  // Hisense TV Power OFF
+  irsend.sendNEC(0x32F1, 32);
+  blinkGreenLED();
+  delay(500);
+
+  // Vizio TV Power OFF
+  irsend.sendNEC(0x20DF01FE, 32);
+  blinkGreenLED();
+  delay(500);
+
+  // Toshiba TV Power OFF (NEC alternatíva)
+  irsend.sendNEC(0x70C0, 32);
+  blinkGreenLED();
+  delay(500);
 }
